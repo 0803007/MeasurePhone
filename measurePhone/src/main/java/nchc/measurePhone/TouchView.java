706165c73c858cbc1d64c8b2317f06fdbc9ee527
0803007 <b90343024@yahoo.com.tw>
2018-01-09 08:59:43 +0800

    update

--- measurePhone/src/main/java/nchc/measurePhone/TouchView.java
+++ measurePhone/src/main/java/nchc/measurePhone/TouchView.java
@@ -172,7 +172,8 @@ public class TouchView extends ImageView {
     private float spacing(MotionEvent event) {
         float x = event.getX(0) - event.getX(1);
         float y = event.getY(0) - event.getY(1);
-        return FloatMath.sqrt(x * x + y * y);
+        //return FloatMath.sqrt(x * x + y * y);
+        return 0;
     }
 
     /**
